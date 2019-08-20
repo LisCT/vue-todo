6e633d5d147101c0721d1918c8fda7d8f616e024
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-08-20 15:32:40 -0500

    adding functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos"/>
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -33,10 +39,24 @@ export default {
       ]
     }
   },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
+    }
+  },
 }
 </script>
 
 <style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="todo-item" v-bind:class="{'is-complete': todo.completed }">
         <p>{{todo.title}}</p>
+        <input type="checkbox" v-bind:checked="todo.completed" v-on:change="markComplete">
+        <button @click="$emit('del-todo', todo.id)" class="del">X</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'TodoItem',
-        props: ['todo']
+        props: ['todo'],
+        methods: {
+            markComplete() {
+               this.todo.completed = !this.todo.completed
+            }
+        },
     }
 </script>
 
